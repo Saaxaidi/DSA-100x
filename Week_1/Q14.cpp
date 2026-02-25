b74ed2c40611b9d884263aa8e25bb9aeb900b957
Syeda Aleema Abbas Zaidi <alimaabbaszaidi100@gmail.com>
2026-02-26 00:00:36 +0530

    3rd commit

---
+++ Week_1/Q14.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long x;
+    long long y;
+    cin>>x>>y;
+
+    if(x==0 and y==0){
+        cout<<"Origin"<<endl;
+    }else if(x!=0 and y==0){
+        cout<<"X axis"<<endl;
+    }else if(x==0 and y!=0){
+        cout<<"Y axis"<<endl;
+    }else if(x>0 and y>0){
+        cout<<"1st Quadrant"<<endl;
+    }else if(x<0 and y>0){
+        cout<<"2nd Quadrant"<<endl;
+    }else if(x<0 and y<0){
+        cout<<"3rd Quadrant"<<endl;
+    }else if(x>0 and y<0){
+        cout<<"4th Quadrant"<<endl;
+    }
+}
